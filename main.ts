8c4630855ab37f4732998916069d56dc58e54996
pc20211052 <115679786+pc20211052@users.noreply.github.com>
2022-10-20 10:46:04 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,14 @@
+let count = 0
 input.onButtonPressed(Button.A, function () {
-	
+    count = -4
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(9)
-    basic.showNumber(8)
-    basic.showNumber(7)
-    basic.showNumber(6)
-    basic.showNumber(5)
-    basic.showNumber(4)
-    basic.showNumber(3)
-    basic.showNumber(2)
-    basic.showNumber(1)
-    basic.showNumber(0)
+    count = 9
+    basic.showNumber(count)
+    while (count > 0) {
+        count += -1
+        basic.showNumber(count)
+    }
     basic.showLeds(`
         . . . . .
         . . . . .
